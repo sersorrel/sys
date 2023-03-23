0973557482093560549e055949505b1b0c6d4ecf
ash <ash@sorrel.sh>
2023-03-23 21:08:38 +0000

    hm: Add TODO for exa

--- hm/exa.nix
+++ hm/exa.nix
@@ -5,7 +5,7 @@
     sys.exa.enable = lib.mkOption {
       description = "Whether to install exa, an ls replacement.";
       type = lib.types.bool;
-      default = true;
+      default = true; # TODO: once nothing implicitly depends on exa, disable this by default
     };
   };
   config = lib.mkIf config.sys.exa.enable {
