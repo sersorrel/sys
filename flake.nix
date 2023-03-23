e0bf72dea8e0f18c8c6a4d8c39c565be9f52339c
ash <ash@sorrel.sh>
2023-03-23 20:59:00 +0000

    Remove explicit lists of top-level imports

--- flake.nix
+++ flake.nix
@@ -5,46 +5,27 @@
 
   };
 
-  outputs = inputs: {
+  outputs = inputs: let
+    mkLib = lib: rec {
+      concatMapAttrsToList = f: attrs: builtins.concatMap (name: f name attrs.${name}) (lib.attrNames attrs); # see lib.mapAttrsToList
+      mkImports = dir: concatMapAttrsToList (name: value: if (value == "regular" && lib.hasSuffix ".nix" name) || (value == "directory" && !lib.hasPrefix "." name) then [(dir + "/${name}")] else []) (builtins.readDir dir);
+    };
+  in {
 
     darwinModules.default = { ... }: {
       _module.args.sysDir = ./.;
     };
 
-    homeModules.default = { ... }: {
+    homeModules.default = { lib, ... }: {
       _module.args.sysDir = ./.;
-      imports = [
-        ./hm/any-nix-shell.nix
-        ./hm/bat.nix
-        ./hm/comma.nix
-        ./hm/dircolors.nix
-        ./hm/direnv.nix
-        ./hm/empty-trash.nix
-        ./hm/exa.nix
-        ./hm/fd.nix
-        ./hm/fish.nix
-        ./hm/fonts.nix
-        ./hm/fzf.nix
-        ./hm/git-fast-forward-all
-        ./hm/git.nix
-        ./hm/less.nix
-        ./hm/neovim
-        ./hm/nix.nix
-        ./hm/ripgrep.nix
-        ./hm/starship.nix
-        ./hm/thumbnailers.nix
-        ./hm/xdg.nix
-      ];
+      imports = (mkLib lib).mkImports ./hm;
       programs.home-manager.enable = true;
       home.enableNixpkgsReleaseCheck = true;
     };
 
-    nixosModules.default = { ... }: {
+    nixosModules.default = { lib, ... }: {
       _module.args.sysDir = ./.;
-      imports = [
-        ./nixos/launchpad.nix
-        ./nixos/sudo-timeout.nix
-      ];
+      imports = (mkLib lib).mkImports ./nixos;
       environment.shellAliases = {
         l = null;
         ll = null;
