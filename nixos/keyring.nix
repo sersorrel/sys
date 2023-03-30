738215ce0811bbb7c96552d32c88733fb518ac69
ash <ash@sorrel.sh>
2023-03-30 19:47:52 +0100

    nixos: Add sys.gnome-keyring.enable

---
+++ nixos/keyring.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.gnome-keyring.enable = lib.mkOption {
+      description = "Whether to enable GNOME Keyring, a Secret Service provider and SSH agent.";
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+  config = {
+    services.gnome.gnome-keyring.enable = true;
+    environment.systemPackages = [ pkgs.gnome.seahorse ];
+  };
+}
