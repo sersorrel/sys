1fb6e397df4d55644b7662889a1e5f7c4eeeba5b
ash <ash@sorrel.sh>
2023-04-02 02:21:16 +0100

    hm: Add sys.dig.enable

---
+++ hm/dig.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.dig.enable = lib.mkOption {
+      description = "Whether to install dig, a command-line DNS lookup utility.";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+  config = {
+    home.packages = [ pkgs.dig.dnsutils ];
+    home.file.".digrc".text = ''
+      +noall +answer +ttlunits +multiline
+    '';
+  };
+}
