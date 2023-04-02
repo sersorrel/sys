985c63440a8dad1d2c3a2339d54d8b098c60646a
ash <ash@sorrel.sh>
2023-04-02 03:05:06 +0100

    hm: Add sys.gnome-music.enable

---
+++ hm/gnome-music.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.gnome-music.enable = lib.mkOption {
+      description = "Whether to install GNOME Music.";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+  config = lib.mkIf config.sys.gnome-music.enable {
+    home.packages = [ pkgs.gnome.gnome-music ];
+  };
+}
