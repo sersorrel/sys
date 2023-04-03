6eaaee48f8254f48e82703e5b8fd3e2be15ac5ad
ash <ash@sorrel.sh>
2023-04-03 19:54:24 +0100

    hm: Add sys.nix-tools.enable

---
+++ hm/nix-tools.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.nix-tools.enable = lib.mkOption {
+      description = "Whether to install various useful Nix-related tools.";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+  config = lib.mkIf config.sys.nix-tools.enable {
+    home.packages = [
+      pkgs.nix-diff
+      pkgs.nix-output-monitor
+      pkgs.expect # for `unbuffer`ing nix-build when piping to nix-output-monitor
+      pkgs.nix-prefetch-github
+      pkgs.nix-prefetch-scripts
+    ];
+  };
+}
