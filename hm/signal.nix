182ae66afaf78ccdaf53168ac9cff5c6e7ad6933
ash <ash@sorrel.sh>
2023-04-03 21:12:17 +0100

    hm: Add sys.signal.enable

---
+++ hm/signal.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.signal.enable = lib.mkOption {
+      description = "Whether to install Signal, a secure messaging client.";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+  config = lib.mkIf config.sys.signal.enable {
+    home.packages = [ pkgs.signal-desktop ];
+  };
+}
