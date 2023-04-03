da33a032d6c8e53ec3df0a7a5e931c912ca0e7cb
ash <ash@sorrel.sh>
2023-04-03 22:58:25 +0100

    nixos: Add sys.avahi.enable

---
+++ nixos/avahi.nix
@@ -0,0 +1,17 @@
+{ config, lib, ... }:
+
+{
+  options = {
+    sys.avahi.enable = lib.mkOption {
+      description = "Whether to enable Avahi, a zeroconf implementation to allow service discovery on the local network.";
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+  config = lib.mkIf config.sys.avahi.enable {
+    services.avahi = {
+      enable = true;
+      nssmdns = true;
+    };
+  };
+}
