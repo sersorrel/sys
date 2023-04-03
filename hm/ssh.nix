0bc7198b48632a55dfb3b58441ed5dec0d2d897c
ash <ash@sorrel.sh>
2023-04-03 21:19:04 +0100

    hm: Add sys.ssh.hosts

--- hm/ssh.nix
+++ hm/ssh.nix
@@ -1,9 +1,21 @@
+{ config, lib, options, pkgs, ... }:
+
 {
-  # This only generates ~/.ssh/config, so there's little point in allowing it to be disabled.
-  programs.ssh = {
-    enable = true;
-    extraConfig = ''
-      AddKeysToAgent yes
-    '';
+  options = {
+    sys.ssh.hosts = lib.mkOption {
+      description = "Per-host SSH settings.";
+      type = options.programs.ssh.matchBlocks.type;
+      default = {};
+    };
+  };
+  config = {
+    # This only generates ~/.ssh/config, so there's little point in allowing it to be disabled.
+    programs.ssh = {
+      enable = true;
+      matchBlocks = config.sys.ssh.hosts;
+      extraConfig = ''
+        AddKeysToAgent yes
+      '';
+    };
   };
 }
