8b6323a3aecb7c4958c20c33e74e429c2c58ee45
ash <ash@sorrel.sh>
2023-04-07 11:53:25 +0100

    hm, nixos: Move sys.thunar.enable from hm to nixos
    This properly enables xfconf, which solves some miscellaneous issues and
    lays the groundwork for solving more.

---
+++ nixos/thunar.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.thunar.enable = lib.mkOption {
+      description = "Whether to install Thunar, a lightweight file manager.";
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+  config = lib.mkIf config.sys.thunar.enable {
+    programs.thunar = {
+      enable = true;
+      plugins = [
+        pkgs.xfce.thunar-archive-plugin
+        pkgs.xfce.thunar-media-tags-plugin
+      ];
+    };
+  };
+}
