28270ae0fcb2f2fa56749dbf0514068d94fa10e3
ash <ash@sorrel.sh>
2023-04-13 12:28:17 +0100

    hm: Add neovim mappings for jumping between snippet placeholders

--- hm/neovim/default.nix
+++ hm/neovim/default.nix
@@ -324,7 +324,15 @@
         cmp-nvim-lsp
         cmp-path
         cmp-vsnip
-        vim-vsnip
+        {
+          plugin = vim-vsnip;
+          config = ''
+            imap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'
+            smap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'
+            imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
+            smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
+          '';
+        }
         {
           plugin = crates-nvim;
           config = ''
