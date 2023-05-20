2df8fbef97d691bb52eadb0926e17f3365884224
ash <ash@sorrel.sh>
2023-05-21 00:43:43 +0100

    hm: Add sys.obs.ftl-servers

--- hm/obs.nix
+++ hm/obs.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+let
+  ftl-config-file = "${config.xdg.configHome}/obs-studio/plugin_config/rtmp-services/services.json";
+in
 {
   options = {
     sys.obs.enable = lib.mkOption {
@@ -7,8 +10,25 @@
       type = lib.types.bool;
       default = false;
     };
+    sys.obs.ftl-servers = lib.mkOption {
+      description = "FTL servers to enable streaming to.";
+      type = lib.types.listOf (lib.types.submodule {
+        options = {
+          name = lib.mkOption {
+            type = lib.types.str;
+          };
+          url = lib.mkOption {
+            type = lib.types.str;
+          };
+        };
+      });
+      default = [];
+    };
   };
   config = lib.mkIf config.sys.obs.enable {
     home.packages = [ pkgs.obs-studio ];
+    home.activation.obsFtl = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD ${pkgs.jq}/bin/jq '.services |= (map(select(.name != "FTL")) | . += [{"name": "FTL", "common": true, "servers": ${builtins.toJSON config.sys.obs.ftl-servers}, "recommended": {"keyint": 2,"output": "ftl_output", "bframes": 0}}])' ${ftl-config-file} | ${pkgs.moreutils}/bin/sponge ${ftl-config-file}
+  '';
   };
 }
