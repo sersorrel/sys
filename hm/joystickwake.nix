63f4a5ac65be084751d1a5ef65a7bde3215620d4
ash <ash@sorrel.sh>
2023-07-04 15:19:48 +0100

    hm: Add sys.joystickwake.enable

---
+++ hm/joystickwake.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    sys.joystickwake.enable = lib.mkOption {
+      description = "Whether to enable joystickwake, a daemon to keep the screen awake when using a game controller.";
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+  config = lib.mkIf config.sys.joystickwake.enable {
+    home.packages = [ pkgs.joystickwake ];
+    # TODO: consider writing a systemd service rather than relying on the provided autostart file
+  };
+}
