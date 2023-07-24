6738fc8c968649f5b0dffec3fb9bb2d63423ce2f
ash <ash@sorrel.sh>
2023-07-24 10:45:09 +0100

    nixos: Add sys.adb.enable

---
+++ nixos/adb.nix
@@ -0,0 +1,15 @@
+{ config, lib, ... }:
+
+{
+  options = {
+    sys.adb.enable = lib.mkOption {
+      description = "Whether to enable ADB, the Android Debug Bridge.";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+  config = lib.mkIf config.sys.adb.enable {
+    programs.adb.enable = true;
+    users.users.ash.extraGroups = [ "adbusers" ];
+  };
+}
