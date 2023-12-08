4e1cd44253eca6b2a4e6b2638afd875f4196a65b
ash <ash@sorrel.sh>
2023-12-08 21:27:16 +0000

    common: Allow unfree packages
    Keeping this list up to date is incredibly onerous.

--- common/nixpkgs/config.nix
+++ common/nixpkgs/config.nix
@@ -1,28 +1,5 @@
 { pkgs, ... }:
 
 {
-  allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-    "android-sdk-tools"
-    "android-studio-stable"
-    "clion"
-    "dataspell"
-    "discord"
-    "discord-ptb"
-    "ffmpeg-full"
-    "google-chrome"
-    "obsidian"
-    "rider"
-    "rust-rover"
-    "slack"
-    "steam"
-    "steam-original"
-    "steam-run"
-    "sublime-merge"
-    "talon"
-    "talon-beta"
-    "todoist-electron"
-    "todoist-electron-8.9.3" # https://github.com/NixOS/nixpkgs/issues/267508
-    "webstorm"
-    "zoom"
-  ];
+  allowUnfree = true;
 }
