fa3b356f2df17412312c930587edd8acdfb65a69
ash <ash@sorrel.sh>
2023-12-19 19:01:07 +0000

    common: Patch gpu-screen-recorder to support security.wrappers

--- common/nixpkgs/overlays.nix
+++ common/nixpkgs/overlays.nix
@@ -24,6 +24,27 @@ in
       ];
     });
     # custom patches
+    gpu-screen-recorder = super.gpu-screen-recorder.overrideAttrs (old: {
+      postInstall = ''
+        install -Dt $out/bin gpu-screen-recorder gsr-kms-server
+        mkdir $out/bin/.wrapped
+        mv $out/bin/gpu-screen-recorder $out/bin/.wrapped/
+        makeWrapper "$out/bin/.wrapped/gpu-screen-recorder" "$out/bin/gpu-screen-recorder" \
+        --prefix LD_LIBRARY_PATH : ${self.libglvnd}/lib \
+        --suffix PATH : $out/bin
+      ''; # prefix -> suffix
+    });
+    gpu-screen-recorder-gtk = super.gpu-screen-recorder-gtk.overrideAttrs (old: {
+      installPhase = ''
+        install -Dt $out/bin/ gpu-screen-recorder-gtk
+        install -Dt $out/share/applications/ gpu-screen-recorder-gtk.desktop
+
+        gappsWrapperArgs+=(--prefix PATH : /run/wrappers/bin:${super.lib.makeBinPath [ self.gpu-screen-recorder ]})
+        # gappsWrapperArgs+=(--prefix PATH : /run/wrappers/bin)
+        # we also append /run/opengl-driver/lib as it otherwise fails to find libcuda.
+        gappsWrapperArgs+=(--prefix LD_LIBRARY_PATH : ${super.lib.makeLibraryPath [ self.libglvnd ]}:/run/opengl-driver/lib)
+      ''; # add /run/wrappers/bin to prefix
+    });
     direnv = super.direnv.overrideAttrs (old: {
       patches = (old.patches or []) ++ [
         (localPath ./patches/direnv-0001-reduce-verbosity.patch)
