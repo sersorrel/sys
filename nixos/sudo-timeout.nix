44370aab232fec4956a95ee86c600185333d6680
ash <ash@sorrel.sh>
2024-01-28 19:40:57 +0000

    nixos: Rename sys.disableSudoTimeout to sys.sudo.disableTimeout

--- nixos/sudo-timeout.nix
+++ nixos/sudo-timeout.nix
@@ -1,14 +1,17 @@
 { config, lib, ... }:
 
 {
+  imports = [
+    (lib.mkRenamedOptionModule [ "sys" "disableSudoTimeout" ] [ "sys" "sudo" "disableTimeout" ])
+  ];
   options = {
-    sys.disableSudoTimeout = lib.mkOption {
+    sys.sudo.disableTimeout = lib.mkOption {
       description = "Whether to disable the sudo password prompt timeout.";
       type = lib.types.bool;
       default = true;
     };
   };
-  config = lib.mkIf config.sys.disableSudoTimeout {
+  config = lib.mkIf config.sys.sudo.disableTimeout {
     security.sudo.extraConfig = ''
       Defaults passwd_timeout=0
     '';
