517f7bae3cc0acc0f3f852164af68346a8c9ea89
ash <ash@sorrel.sh>
2024-02-19 17:49:06 +0000

    hm: Glimmer 2.10.34 -> 2.10.36

--- hm/glimmer/glimmer.nix
+++ hm/glimmer/glimmer.nix
@@ -46,6 +46,7 @@
 , libheif
 , libgudev
 , openexr
+, desktopToDarwinBundle
 , AppKit
 , Cocoa
 , gtk-mac-integration-gtk2
@@ -55,15 +56,15 @@
 
 let
   python = python2.withPackages (pp: [ pp.pygtk ]);
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "glimmer";
-  version = "2.10.34";
+  version = "2.10.36";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
-    url = "http://download.gimp.org/pub/gimp/v${lib.versions.majorMinor version}/gimp-${version}.tar.bz2";
-    sha256 = "hABGQtNRs5ikKTzX/TWSBEqUTwW7UoUO5gaPJHxleqM=";
+    url = "http://download.gimp.org/pub/gimp/v${lib.versions.majorMinor finalAttrs.version}/gimp-${finalAttrs.version}.tar.bz2";
+    sha256 = "sha256-PTvDxppL2zrqm6LVOF7ZjqA5U/OFeq/R1pdgEe1827I=";
   };
 
   patches = [
@@ -89,6 +90,8 @@ in stdenv.mkDerivation rec {
     makeWrapper
     gtk-doc
     rename
+  ] ++ lib.optionals stdenv.isDarwin [
+    desktopToDarwinBundle
   ];
 
   buildInputs = [
@@ -160,55 +163,57 @@ in stdenv.mkDerivation rec {
 
   doCheck = true;
 
-  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin "-DGDK_OSX_BIG_SUR=16";
+  env = {
+    NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin "-DGDK_OSX_BIG_SUR=16";
 
-  # Check if librsvg was built with --disable-pixbuf-loader.
-  PKG_CONFIG_GDK_PIXBUF_2_0_GDK_PIXBUF_MODULEDIR = "${librsvg}/${gdk-pixbuf.moduleDir}";
+    # Check if librsvg was built with --disable-pixbuf-loader.
+    PKG_CONFIG_GDK_PIXBUF_2_0_GDK_PIXBUF_MODULEDIR = "${librsvg}/${gdk-pixbuf.moduleDir}";
+  };
 
   postUnpack = let
     ignoredFiles = "\\! -name '*.png' \\! -name '*.xcf' \\! -name '*.gih' \\! -name '*.gbr' \\! -name '*.pat' \\! -name '*.pgm' \\! -name '*.ppm'";
     # ggr, gpl, scm, gdyn, gtp, vbr are text files
     # this sucks!
   in ''
-    mv gimp-${version} glimmer-${version}
-    sourceRoot="glimmer-${version}"
+    mv gimp-${finalAttrs.version} glimmer-${finalAttrs.version}
+    sourceRoot="glimmer-${finalAttrs.version}"
     echo "no it's not, it's $sourceRoot"
 
-    sed -i 's/GiMP/Glimmer/g' glimmer-${version}/po-plug-ins/sk.po # upstream typo?
+    sed -i 's/GiMP/Glimmer/g' glimmer-${finalAttrs.version}/po-plug-ins/sk.po # upstream typo?
     # remove obsolete entries from some po files, because they cause conflicts when the case is squashed
-    sed -i '/^#~/d' glimmer-${version}/po-plug-ins/*.po glimmer-${version}/po-tips/*.po
+    sed -i '/^#~/d' glimmer-${finalAttrs.version}/po-plug-ins/*.po glimmer-${finalAttrs.version}/po-tips/*.po
 
     # GIMP_ -> GLIMMER_: leave stuff in code in allcaps
     # GIMP/ -> GLIMMER/: see e.g. line 446 of pdb/app.pl (without the capture group, this is a very bad pattern! it would match e.g. the definition of GLIMMER_DBUS_INTERFACE_PATH, which should *not* be all-caps)
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/gimp(?! xcf)/glimmer/g' {} + # avoid changing the XCF file magic number
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/Gimp/Glimmer/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/GIMP_/GLIMMER_/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's@(s/(\\*[^\\/]|(\\\\)*/)*)GIMP/@\1GLIMMER/@g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/GIMP/Glimmer/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/GNU Image Manipulation Program/Glimmer/g' {} +
-    find glimmer-${version} -depth -execdir rename 's/gimp/glimmer/g' {} +
-    find glimmer-${version} -depth -execdir rename 's/Gimp/Glimmer/g' {} +
-    find glimmer-${version} -depth -execdir rename 's/GIMP_/GLIMMER_/g' {} +
-    find glimmer-${version} -depth -execdir rename 's/GIMP/Glimmer/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/gimp(?! xcf)/glimmer/g' {} + # avoid changing the XCF file magic number
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/Gimp/Glimmer/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/GIMP_/GLIMMER_/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's@(s/(\\*[^\\/]|(\\\\)*/)*)GIMP/@\1GLIMMER/@g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/GIMP/Glimmer/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/GNU Image Manipulation Program/Glimmer/g' {} +
+    find glimmer-${finalAttrs.version} -depth -execdir rename 's/gimp/glimmer/g' {} +
+    find glimmer-${finalAttrs.version} -depth -execdir rename 's/Gimp/Glimmer/g' {} +
+    find glimmer-${finalAttrs.version} -depth -execdir rename 's/GIMP_/GLIMMER_/g' {} +
+    find glimmer-${finalAttrs.version} -depth -execdir rename 's/GIMP/Glimmer/g' {} +
 
     # fix up a test, which expects to see specific strings saved in an XCF file saved to source control
-    sed -i 's@app/tests/test-xcf.c in the Glimmer@app/tests/test-xcf.c in the GIMP@g' glimmer-${version}/app/tests/test-xcf.c
-    sed -i 's/opened and inspected in Glimmer/opened and inspected in GIMP/g' glimmer-${version}/app/tests/test-xcf.c
+    sed -i 's@app/tests/test-xcf.c in the Glimmer@app/tests/test-xcf.c in the GIMP@g' glimmer-${finalAttrs.version}/app/tests/test-xcf.c
+    sed -i 's/opened and inspected in Glimmer/opened and inspected in GIMP/g' glimmer-${finalAttrs.version}/app/tests/test-xcf.c
 
     # fix known parasites from devel-docs/parasites.txt
     # the lookaheads avoid matching filenames
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-comment(?!\.)/gimp-comment/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-brush-name(?!\.)/gimp-brush-name/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-brush-pipe-name(?!\.)/gimp-brush-pipe-name/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-brush-pipe-parameters(?!\.)/gimp-brush-pipe-parameters/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-image-grid(?!\.)/gimp-image-grid/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-pattern-name(?!\.)/gimp-pattern-name/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-metadata(?!\.)/gimp-metadata/g' {} +
-    find glimmer-${version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-text-layer(?!\.)/gimp-text-layer/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-comment(?!\.)/gimp-comment/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-brush-name(?!\.)/gimp-brush-name/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-brush-pipe-name(?!\.)/gimp-brush-pipe-name/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-brush-pipe-parameters(?!\.)/gimp-brush-pipe-parameters/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-image-grid(?!\.)/gimp-image-grid/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-pattern-name(?!\.)/gimp-pattern-name/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-metadata(?!\.)/gimp-metadata/g' {} +
+    find glimmer-${finalAttrs.version} -type f ${ignoredFiles} -exec perl -i -pe 's/glimmer-text-layer(?!\.)/gimp-text-layer/g' {} +
 
     # attempt to delete wilber (this doesn't work)
-    # sed -i 's/wilber_path\[] = ".*";/wilber_path[] = "";/g' glimmer-${version}/app/widgets/glimmercairo-wilber.c
-    # sed -i 's/eyes_path\[] = ".*";/eyes_path[] = "";/g' glimmer-${version}/app/widgets/glimmercairo-wilber.c
+    # sed -i 's/wilber_path\[] = ".*";/wilber_path[] = "";/g' glimmer-${finalAttrs.version}/app/widgets/glimmercairo-wilber.c
+    # sed -i 's/eyes_path\[] = ".*";/eyes_path[] = "";/g' glimmer-${finalAttrs.version}/app/widgets/glimmercairo-wilber.c
   '';
 
   preConfigure = ''
@@ -217,21 +222,23 @@ in stdenv.mkDerivation rec {
   '';
 
   postFixup = ''
-    wrapProgram $out/bin/glimmer-${lib.versions.majorMinor version} \
+    wrapProgram $out/bin/glimmer-${lib.versions.majorMinor finalAttrs.version} \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE"
   '';
 
-  passthru = rec {
+  passthru = {
     # The declarations for `glimmer-with-plugins` wrapper,
     # used for determining plug-in installation paths
-    majorVersion = "${lib.versions.major version}.0";
-    targetLibDir = "lib/glimmer/${majorVersion}";
-    targetDataDir = "share/glimmer/${majorVersion}";
-    targetPluginDir = "${targetLibDir}/plug-ins";
-    targetScriptDir = "${targetDataDir}/scripts";
+    majorVersion = "${lib.versions.major finalAttrs.version}.0";
+    targetLibDir = "lib/glimmer/${finalAttrs.passthru.majorVersion}";
+    targetDataDir = "share/glimmer/${finalAttrs.passthru.majorVersion}";
+    targetPluginDir = "${finalAttrs.passthru.targetLibDir}/plug-ins";
+    targetScriptDir = "${finalAttrs.passthru.targetDataDir}/scripts";
 
     # probably its a good idea to use the same gtk in plugins ?
     gtk = gtk2;
+
+    python2Support = withPython;
   };
 
   meta = with lib; {
@@ -241,4 +248,4 @@ in stdenv.mkDerivation rec {
     platforms = platforms.unix;
     mainProgram = "glimmer";
   };
-}
+})
