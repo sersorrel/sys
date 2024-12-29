830afd35d92665edcb33c348b60e19584eff5e0e
ash <ash@sorrel.sh>
2024-12-29 21:31:14 +0000

    nixos: Add fallback NetworkManager config

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,3 +1,4 @@
 {
   networking.networkmanager.connectionConfig."connection.mdns" = 2; # aka "yes", i.e. "register hostname and resolving for the connection", see nm-settings(5)
+  networking.networkmanager.connectionConfig."mdns" = 2; # unclear whether this or the above is the correct syntax
 }
