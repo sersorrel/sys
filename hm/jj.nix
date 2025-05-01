80b7ffc428b78b3e38c5b8a3217139bea753df0b
ash <ash@sorrel.sh>
2025-05-01 21:04:48 +0100

    hm: Add more jj aliases

--- hm/jj.nix
+++ hm/jj.nix
@@ -16,6 +16,8 @@
         aliases = {
           aa = [ "log" "-r" "all()" ];
           blt = [ "b" "l" "-t" ];
+          pog = [ "op" "log" "-p" ];
+          shop = [ "op" "show" "-p" ];
           tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
           unwip = [ "util" "exec" "--" "bash" "-c" ''
             jj log --no-graph -r "''${1:-@}" -T description | sed -e 's/^wip: //' | jj desc --stdin "''${1:-@}"
