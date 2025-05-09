2f507072cc862eb1c3ac317338daa1858bf0400f
ash <x-hv2j5m@keysmash.solutions>
2025-05-09 08:59:50 +0100

    hm: Use same contrast for neovim in both light and dark mode

--- hm/neovim/default.nix
+++ hm/neovim/default.nix
@@ -113,6 +113,7 @@
           config = ''
             let g:gruvbox_italic = 1
             let g:gruvbox_contrast_light = 'hard'
+            let g:gruvbox_contrast_dark = 'hard'
             let g:gruvbox_invert_selection = 0
             colorscheme gruvbox
           '';
