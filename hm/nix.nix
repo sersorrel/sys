9bca43b37c0e692c6b27e05671f0790c1b106c14
ash <x-hv2j5m@keysmash.solutions>
2025-05-11 15:46:52 +0100

    hm: Use `some` as $NIX_PAGER

--- hm/nix.nix
+++ hm/nix.nix
@@ -1,9 +1,8 @@
 { config, inputs, pkgs, sysDir, unstable, ... }:
 
 {
-  # Don't pipe stuff like nix-store output (which is frequently a single line) through a pager.
-  # NB: this doesn't actually use cat(1); nix checks specifically for the string "cat" and disables the pager in that case.
-  home.sessionVariables.NIX_PAGER = "cat";
+  # Don't show stuff like nix-store output (which is frequently a single line) in less.
+  home.sessionVariables.NIX_PAGER = "some";
 
   nix.registry = {
     nixpkgs.flake = inputs.nixpkgs;
